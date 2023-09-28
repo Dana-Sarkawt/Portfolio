248a8d5ffe2ad40af0f8cec7a1227d0c890449e0
Dana-Sarkawt <91015045+Dana-Sarkawt@users.noreply.github.com>
2023-09-28 15:26:39 +0300

    update

--- src/lib/component/courses.svelte
+++ src/lib/component/courses.svelte
@@ -3,10 +3,10 @@
   </script>
 
 <div class="w-full h-auto flex items-center mt-10 ">
-<Marquee shadow class="py-5 " speed={1} hoverSpeed={0.5} >
-    <img class="w-24 h-24 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rounded-2xl " alt="flowbite-svelte icon logo" src="../img/html-course.jpg" />
-    <img class="w-24 h-24 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rounded-2xl" alt="flowbite-svelte icon logo" src="../img/css-course.jpg" />
-    <img class="w-24 h-24 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rounded-2xl" alt="flowbite-svelte icon logo" src="../img/responsive.jpg" />
+<Marquee shadow class="py-5 " speed={0.7} hoverSpeed={0.2} >
+    <img class="w-32 h-32 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rounded-2xl " alt="flowbite-svelte icon logo" src="../img/html-course.jpg" />
+    <img class="w-32 h-32 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rounded-2xl" alt="flowbite-svelte icon logo" src="../img/css-course.jpg" />
+    <img class="w-32 h-32 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rounded-2xl" alt="flowbite-svelte icon logo" src="../img/responsive.jpg" />
 </Marquee>
 
 </div>
